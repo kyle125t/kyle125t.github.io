d3dc28b973a2220470c2a2a30ac429fc4a4385b5
kyle125t <73258610+kyle125t@users.noreply.github.com>
2022-08-19 09:56:34 -0400

    Attempting to import from csv

---
+++ Bricksquad FF/main-data.js
@@ -0,0 +1,38 @@
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+  top: 30,
+  right: 30,
+  bottom: 30,
+  left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
+// to the margins set in the "chartMargin" object.
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from csv
+d3.csv('973BSFF-Combined Sheet.csv').then(function(ffData) {
+
+  // Print the tvData
+  console.log(ffData);
+
+  // Cast the hours value to a number for each piece of tvData
+  ffData.forEach(function(data) {
+    data.name = +data.name;
+  })}); 
